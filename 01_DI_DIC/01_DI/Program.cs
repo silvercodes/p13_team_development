b3c0990e6fcf2f0f2cb0082ce577e80081680637
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2024-03-27 22:36:36 +0300

    add: DI, DIP

---
+++ 01_DI_DIC/01_DI/Program.cs
@@ -0,0 +1,50 @@
+﻿
+Injector injector = new Injector();
+Client client = injector.ResolveClient();
+client.RunService();
+
+
+interface IService
+{
+    public void Execute();
+}
+
+class ServiceOne : IService
+{
+    public void Execute()
+    {
+        Console.WriteLine("ServiceOne::Execute()");
+    }
+}
+
+class ServiceTwo : IService
+{
+    public void Execute()
+    {
+        Console.WriteLine("ServiceTwo::Execute()");
+    }
+}
+
+class Client
+{
+    private IService service;
+
+    public Client(IService service)
+    {
+        this.service = service;
+    }
+    public void RunService()
+    {
+        service.Execute();
+    }
+}
+
+class Injector
+{
+    public Client ResolveClient()
+    {
+        ServiceTwo service = new ServiceTwo();
+        Client client = new Client(service);            // DI
+        return client;
+    }
+}
